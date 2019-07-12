80592d55fa5adfe8595b0ca36d39649d7d110501
Sijan <sijanshrestha2@gmail.com>
2019-07-12 13:46:40 +0700

    chore: working configs

--- config/webpack/server.js
+++ config/webpack/server.js
@@ -79,6 +79,5 @@ var config = {
 utils.copySyncIfDoesntExist('./config/main.js', './config/main.local.js');
 utils.createIfDoesntExist('./build');
 utils.createIfDoesntExist('./build/public');
-utils.copySync('./src/favicon.ico', './build/public/favicon.ico', true);
 
 module.exports = config;
--- src/app/redux/rootReducer.ts
+++ src/app/redux/rootReducer.ts
@@ -1,10 +1,12 @@
 import {combineReducers, Reducer} from "redux";
+import {router5Reducer} from "redux-router5";
 import {IStore} from "./IStore";
 import {counterReducer} from "./modules/counterModule";
 
 const rootReducer: Reducer<IStore> = combineReducers<IStore>(
     {
-        counter: counterReducer
+        counter: counterReducer,
+        router: router5Reducer
     });
 
 export default rootReducer;
